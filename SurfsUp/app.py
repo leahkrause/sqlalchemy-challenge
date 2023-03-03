5923a3a0be67ea916fe6892731a288fa192533b9
leahkrause <leahrkrause@gmail.com>
2023-03-02 18:16:24 -0800

    climate app updates

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
 
+# Import dependencies
+from flask import Flask
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# Set up database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table 
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Flask set up
 app = Flask(__name__)
 
+# Homepage 
 @app.route("/")
 def homepage():
     return """Welcome to Leah's climate app. Please select from the below:
@@ -11,3 +33,5 @@ def homepage():
     <br><a> href="/api/v1.0/<start>/<end>">Start and End (JSON)</a>
     """
 
+@app.route("/api/v1.0/precipitation")
+def precipitation()
