c13b48f2a6358d5947d5ac6857683cf475d62842
leahkrause <leahrkrause@gmail.com>
2023-03-07 21:26:38 -0800

    updates

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,6 @@
 
 # Import dependencies
-from flask import Flask
+from flask import Flask, jsonify
 import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -34,4 +34,68 @@ def homepage():
     """
 
 @app.route("/api/v1.0/precipitation")
-def precipitation()
+def precipitation():
+    session = Session(engine)
+
+    # Find the most recent date in the data set.
+    recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+    # Starting from the most recent data point in the database. 
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    data = session.query(measurement.date, measurement.prcp).\
+    filter(measurement.date > (year_ago)).order_by(measurement.date.desc())
+  
+    session.close()
+
+    # Create a dictionary using date as the key and prcp as the value
+    all_precipitation = []
+    for date, prcp in data:
+        prcp_dict = {}
+        prcp_dict["Date"] = date
+        prcp_dict["Precipitation"] = prcp
+        all_precipitation.append(prcp_dict)
+
+    return jsonify(all_precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    # Find all distinct stations
+    tot_stations = session.query(station).distinct()
+
+    session.close()
+
+    return jsonify(tot_stations)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    # Query dates and temperature observations of the most-active station for the previous year of data
+    sel = [station.station, func.count(measurement.station)]
+    active_query = session.query(*sel).filter(station.station == measurement.station).group_by(station.station).\
+    order_by(func.count(measurement.station).desc()).all()
+
+    sel = [func.min(measurement.tobs),
+      func.max(measurement.tobs),
+      func.avg(measurement.tobs)]
+
+    active_data = session.query(*sel).filter(measurement.station == 'USC00519281').all()
+
+    last_year_data = session.query(measurement.date, measurement.tobs).filter(measurement.station == 'USC00519281', measurement.date >= year_ago).all()
+    df_last_year = pd.DataFrame(last_year_data, columns=['date', 'temperature'])
+
+    session.close()
+
+    return jsonify(df_last_year)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
